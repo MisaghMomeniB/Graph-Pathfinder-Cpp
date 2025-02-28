f98bc62bc667a51cb77e836f52a07a3752621c83
MisaghMomeniB <jenabemisagh@gmail.com>
2025-02-28 12:52:09 +0330

    struct node

--- src/main.cpp
+++ src/main.cpp
@@ -10,3 +10,11 @@ using namespace std;
 struct Edge {
     int to, weight;
 };
+
+struct Node {
+    int id;
+    double cost;
+    bool operator>(const Node &other) const {
+        return cost > other.cost;
+    }
+};
