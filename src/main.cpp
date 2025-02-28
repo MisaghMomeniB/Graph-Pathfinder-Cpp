b32ac0af16fe4eff34cfbfa62b7ec7bfe9fc9af9
MisaghMomeniB <jenabemisagh@gmail.com>
2025-02-28 14:26:33 +0330

    optimized dijkstra's algorithm, added floyed warshall algorithm, input from file

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,11 @@
 #include <queue>
 #include <unordered_map>
 #include <unordered_set>
+#include <set>
 #include <cmath>
 #include <limits>
 #include <algorithm>
+#include <fstream>
 
 using namespace std;
 
@@ -46,35 +48,31 @@ public:
     vector<int> dijkstra(int start, int goal) {
         unordered_map<int, int> parent;
         unordered_map<int, double> dist;
-        unordered_set<int> visited;
-        priority_queue<Node, vector<Node>, greater<Node>> pq;
+        set<pair<double, int>> pq;
 
         for (auto &[node, _] : adj) {
             dist[node] = numeric_limits<double>::infinity();
         }
-
         dist[start] = 0;
-        pq.push({start, 0});
+        pq.insert({0, start});
 
         while (!pq.empty()) {
-            int current = pq.top().id;
-            pq.pop();
-
-            if (visited.find(current) != visited.end()) continue;
-            visited.insert(current);
+            int current = pq.begin()->second;
+            pq.erase(pq.begin());
 
             if (current == goal) break;
 
             for (const auto &edge : adj[current]) {
                 double newDist = dist[current] + edge.weight;
                 if (newDist < dist[edge.to]) {
+                    pq.erase({dist[edge.to], edge.to});
                     dist[edge.to] = newDist;
                     parent[edge.to] = current;
-                    pq.push({edge.to, newDist});
+                    pq.insert({newDist, edge.to});
                 }
             }
         }
-
+        
         vector<int> path;
         for (int at = goal; at != start; at = parent[at]) {
             if (parent.find(at) == parent.end()) return {};
@@ -85,141 +83,74 @@ public:
         return path;
     }
 
-    vector<int> aStar(int start, int goal, unordered_map<int, pair<int, int>> &coordinates) {
-        auto heuristic = [&](int a, int b) {
-            return sqrt(pow(coordinates[a].first - coordinates[b].first, 2) +
-                        pow(coordinates[a].second - coordinates[b].second, 2));
-        };
-
-        unordered_map<int, int> parent;
-        unordered_map<int, double> gScore, fScore;
-        unordered_set<int> visited;
-        priority_queue<Node, vector<Node>, greater<Node>> pq;
-
-        for (auto &[node, _] : adj) {
-            gScore[node] = numeric_limits<double>::infinity();
-            fScore[node] = numeric_limits<double>::infinity();
-        }
-
-        gScore[start] = 0;
-        fScore[start] = heuristic(start, goal);
-        pq.push({start, fScore[start]});
-
-        while (!pq.empty()) {
-            int current = pq.top().id;
-            pq.pop();
-
-            if (visited.find(current) != visited.end()) continue;
-            visited.insert(current);
-
-            if (current == goal) break;
-
-            for (const auto &edge : adj[current]) {
-                double tentative_gScore = gScore[current] + edge.weight;
-                if (tentative_gScore < gScore[edge.to]) {
-                    parent[edge.to] = current;
-                    gScore[edge.to] = tentative_gScore;
-                    fScore[edge.to] = gScore[edge.to] + heuristic(edge.to, goal);
-                    pq.push({edge.to, fScore[edge.to]});
-                }
+    void floydWarshall(int n) {
+        vector<vector<double>> dist(n, vector<double>(n, numeric_limits<double>::infinity()));
+        for (int i = 0; i < n; i++) dist[i][i] = 0;
+        
+        for (auto &[u, edges] : adj) {
+            for (auto &edge : edges) {
+                dist[u][edge.to] = edge.weight;
             }
         }
-
-        vector<int> path;
-        for (int at = goal; at != start; at = parent[at]) {
-            if (parent.find(at) == parent.end()) return {};
-            path.push_back(at);
-        }
-        path.push_back(start);
-        reverse(path.begin(), path.end());
-        return path;
-    }
-
-    vector<int> bellmanFord(int start, int goal) {
-        unordered_map<int, double> dist;
-        unordered_map<int, int> parent;
-
-        for (auto &[node, _] : adj) {
-            dist[node] = numeric_limits<double>::infinity();
-        }
-
-        dist[start] = 0;
-
-        for (size_t i = 0; i < adj.size() - 1; i++) {
-            for (auto &[u, edges] : adj) {
-                for (auto &edge : edges) {
-                    if (dist[u] + edge.weight < dist[edge.to]) {
-                        dist[edge.to] = dist[u] + edge.weight;
-                        parent[edge.to] = u;
+        
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (dist[i][k] != numeric_limits<double>::infinity() &&
+                        dist[k][j] != numeric_limits<double>::infinity() &&
+                        dist[i][j] > dist[i][k] + dist[k][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
                     }
                 }
             }
         }
-
-        for (auto &[u, edges] : adj) {
-            for (auto &edge : edges) {
-                if (dist[u] + edge.weight < dist[edge.to]) {
-                    cout << "Negative cycle detected!" << endl;
-                    return {};
-                }
+        
+        cout << "Shortest distances between every pair of vertices:\n";
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dist[i][j] == numeric_limits<double>::infinity())
+                    cout << "INF ";
+                else
+                    cout << dist[i][j] << " ";
             }
+            cout << endl;
         }
-
-        vector<int> path;
-        for (int at = goal; at != start; at = parent[at]) {
-            if (parent.find(at) == parent.end()) return {};
-            path.push_back(at);
-        }
-        path.push_back(start);
-        reverse(path.begin(), path.end());
-        return path;
     }
 };
 
 int main() {
     Graph graph;
-    int edges, u, v, w;
+    int edges, u, v, w, nodes;
     bool directed;
     
-    cout << "Enter number of edges: ";
-    cin >> edges;
-    cout << "Is the graph directed? (1 for Yes, 0 for No): ";
-    cin >> directed;
-
-    cout << "Enter edges (u v w):\n";
+    ifstream inputFile("graph_input.txt");
+    if (!inputFile) {
+        cout << "Error opening file!" << endl;
+        return 1;
+    }
+    
+    inputFile >> nodes >> edges >> directed;
+    
     for (int i = 0; i < edges; i++) {
-        cin >> u >> v >> w;
+        inputFile >> u >> v >> w;
         graph.addEdge(u, v, w, directed);
     }
-
-    unordered_map<int, pair<int, int>> coordinates;
-    cout << "Enter node coordinates (id x y):\n";
-    for (int i = 0; i < edges + 1; i++) {
-        int id, x, y;
-        cin >> id >> x >> y;
-        coordinates[id] = {x, y};
-    }
+    inputFile.close();
 
     int start, goal;
     cout << "Enter start and goal nodes: ";
     cin >> start >> goal;
 
-    graph.printGraph();
-
     vector<int> pathDijkstra = graph.dijkstra(start, goal);
-    vector<int> pathAStar = graph.aStar(start, goal, coordinates);
-    vector<int> pathBellmanFord = graph.bellmanFord(start, goal);
-
+    graph.floydWarshall(nodes);
+    
     auto printPath = [](const string &name, const vector<int> &path) {
         cout << name << " Path: ";
         if (path.empty()) cout << "No path found!";
         for (int node : path) cout << node << " ";
         cout << endl;
     };
-
+    
     printPath("Dijkstra", pathDijkstra);
-    printPath("A*", pathAStar);
-    printPath("Bellman-Ford", pathBellmanFord);
-
     return 0;
 }
