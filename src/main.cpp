1dd307a2f869f94c72948b27923c86786c0487a5
MisaghMomeniB <jenabemisagh@gmail.com>
2025-02-28 12:54:03 +0330

    vector, int

--- src/main.cpp
+++ src/main.cpp
@@ -26,3 +26,30 @@ public:
         adj[u].push_back({v, w});
         adj[v].push_back({u, w});
     }
+
+    vector<int> dijkstra(int start, int goal) {
+        unordered_map<int, int> parent;
+        unordered_map<int, double> dist;
+        priority_queue<Node, vector<Node>, greater<Node>> pq;
+        
+        for (auto &[node, _] : adj) dist[node] = numeric_limits<double>::infinity();
+        
+        dist[start] = 0;
+        pq.push({start, 0});
+        
+        while (!pq.empty()) {
+            int current = pq.top().id;
+            double currentCost = pq.top().cost;
+            pq.pop();
+            
+            if (current == goal) break;
+            
+            for (const auto &edge : adj[current]) {
+                double newDist = currentCost + edge.weight;
+                if (newDist < dist[edge.to]) {
+                    dist[edge.to] = newDist;
+                    parent[edge.to] = current;
+                    pq.push({edge.to, newDist});
+                }
+            }
+        }
